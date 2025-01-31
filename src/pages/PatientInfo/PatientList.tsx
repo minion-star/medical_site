96255c5cf048e64c2478c998369460d1ab95ab0a
William Alderette <williamalderette799@gmail.com>
2025-01-31 10:43:14 -0600

    Update PatientList.tsx

--- src/pages/PatientInfo/PatientList.tsx
+++ src/pages/PatientInfo/PatientList.tsx
@@ -1,145 +1,63 @@
-import React, { useContext, useState } from "react";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Paper,
-  Grid,
-  Box,
-  TablePagination,
-  Toolbar,
-  Container,
-  CircularProgress,
-} from "@mui/material";
-import { Link } from "react-router-dom";
-import Chip from "@mui/material/Chip";
-import Appbar from "../../components/Appbar";
-import { PatientListContext } from "../../contexts/PatientListContext";
-import AddPatientDialog from "./AddPatientDialog";
-import { useNavigate } from "react-router-dom";
+import * as React from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import Slide from "@mui/material/Slide";
+import { TransitionProps } from "@mui/material/transitions";
+import { Stack } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import SearchInput from "../../components/SearchInput";
+import { useForm } from "react-hook-form";
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import axios from "axios";
 
 
+export default function AddPatientDialog({
 
-const PatientList: React.FC = () => {
-  // Access patient data from the context
-  const patientList = useContext(PatientListContext);
-  const Navigate = useNavigate();
+  handleChange
+}: any) {
 
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(5);
+  
+  const handleAddPatient = async()=>{
+    try {
+      // Sending a request to add a blank row to the database
+      const response = await axios.post("http://localhost:5000/api/addBlankPatient");
 
-  if (!patientList) {
-    return (
-      <Box sx={{ display: "flex", justifyContent: "center", mt: 4 }}>
-        <CircularProgress color="success" />
-      </Box>
-    );
+      if (response.status === 200) {
+        handleChange(); // Call this function to update the state or refresh the table
+      }
+    } catch (error) {
+      console.error("There was an error adding the blank patient:", error);
+    }
   }
 
-  const handleChangePage = (
-    event: React.MouseEvent<HTMLButtonElement> | null,
-    newPage: number
-  ) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0); // Reset page to the first page when changing rows per page
-  };
-
-  const emptyRows =
-    rowsPerPage -
-    Math.min(rowsPerPage, patientList.length - page * rowsPerPage);
-
   return (
-    <Box sx={{ display: "flex" }}>
-      <Appbar appBarTitle="Patient List" />
-      <Box
-        component="main"
-        sx={{
-          backgroundColor: (theme) =>
-            theme.palette.mode === "light"
-              ? theme.palette.grey[100]
-              : theme.palette.grey[900],
-          flexGrow: 1,
-          height: "100vh",
-          overflow: "auto",
-        }}
+    <div>
+      <Stack
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+        spacing={2}
       >
-        <Toolbar />
-
-        <Container sx={{ mt: 4, mb: 4 }}>
-          <AddPatientDialog/>
-          <Grid container spacing={2} sx={{ marginLeft: "10px", marginTop: "40px" }}>
-            <TableContainer component={Paper}>
-              <Table sx={{ minWidth: 650 }} aria-label="patient table">
-                <TableHead>
-                  <TableRow>
-                    <TableCell align="center">CSN</TableCell>
-                    <TableCell>FULL NAME</TableCell>
-                    <TableCell>AGE</TableCell>
-                    <TableCell>GENDER</TableCell>
-                    <TableCell>CREATED DATE</TableCell>
-                    <TableCell>CREATED BY</TableCell>
-                    <TableCell>LAST SAVED DATE</TableCell>
-                    <TableCell>LAST SAVED BY</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {(rowsPerPage > 0
-                    ? patientList.slice(
-                        page * rowsPerPage,
-                        page * rowsPerPage + rowsPerPage
-                      )
-                    : patientList
-                  ).map((patient, index) => (
-                    <TableRow
-                      key={index}
-                      hover
-                      onClick={() => Navigate(`/patient-info/${patient.CSN}`)}
-                      style={{ cursor: "pointer" }}
-                    >
-                      <TableCell align="center">{patient.CSN}</TableCell>
-                      <TableCell>
-                        {patient.FIRST_NAME} {patient.LAST_NAME}
-                      </TableCell>
-                      <TableCell>{patient.AGE}</TableCell>
-                      <TableCell>{patient.INFORMATION.personalInformation.gender}</TableCell>
-                      <TableCell>{patient.CREATED_DATE}</TableCell>
-                      <TableCell>{patient.CREATED_BY}</TableCell>
-                      <TableCell>{patient.LAST_SAVED_DATE}</TableCell>
-                      <TableCell>{patient.LAST_SAVED_BY}</TableCell>
-                    </TableRow>
-                  ))}
-                  {emptyRows > 0 && (
-                    <TableRow style={{ height: 53 * emptyRows }}>
-                      <TableCell colSpan={8} />
-                    </TableRow>
-                  )}
-                </TableBody>
-              </Table>
-            </TableContainer>
-
-            <TablePagination
-              rowsPerPageOptions={[5, 10, 25]}
-              component="div"
-              count={patientList.length}
-              rowsPerPage={rowsPerPage}
-              page={page}
-              onPageChange={handleChangePage}
-              onRowsPerPageChange={handleChangeRowsPerPage}
-            />
-          </Grid>
-        </Container>
-      </Box>
-    </Box>
+        <SearchInput handleChange={handleChange} />
+        <Button
+          variant="outlined"
+          startIcon={<AddIcon />}
+          onClick={handleAddPatient}
+        >
+          Add Patient
+        </Button>
+      </Stack>
+    </div>
   );
-};
-
-export default PatientList;
+}
