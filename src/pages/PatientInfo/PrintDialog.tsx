8b2af0c9433e57c9f3612b5153a877fb9eaab361
minion-star <williamalderette799@gmail.com>
2025-02-10 03:40:23 -0600

    update

--- src/pages/PatientInfo/PrintDialog.tsx
+++ src/pages/PatientInfo/PrintDialog.tsx
@@ -461,7 +461,7 @@ const PrintDialog = (props:{open:boolean; handleClose:any;}) => {
             }}
             
           > 
-            <Tabs orientation="vertical" sx={{borderRight: 1, borderColor: "divider", width:210, height:"800px"}} variant="scrollable">
+            <Tabs orientation="vertical" sx={{borderRight: 1, borderColor: "divider", width:210, height:"1000px"}} variant="scrollable">
                 <Tab label="Laboratory Order"/>
                 <Tab label="Radiology Order "/>
                 <Tab label="General Order" sx={{borderBottom:1, borderColor:"divider"}}/>
